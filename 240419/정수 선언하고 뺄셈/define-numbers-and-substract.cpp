7a2769450750d4111e794bf1242abb4b298a419c
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-04-19 17:12:06 +0900

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240419/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 97;
+    int num2 = 13;
+    int num3 = num1 - num2;
+    cout<<num1; cout<<" - "; cout<<num2; cout<<" = "; cout<<num3;
+    return 0;
+}
