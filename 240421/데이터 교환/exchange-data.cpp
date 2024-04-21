0eb556ae9a5b9ad7d926d4f5c9ed84a27b7e0cde
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-04-21 18:11:07 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240421/데이터 교환/exchange-data.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 6;
+    int c = 7;
+    int temp = c;
+    c = b;
+    b = a;
+    a = temp;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c;
+    return 0;
+}
