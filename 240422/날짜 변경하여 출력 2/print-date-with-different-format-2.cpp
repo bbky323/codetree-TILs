51eb3139cf65cd4a20204865698c762499cd97f9
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-04-22 19:06:13 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240422/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, d, y;
+    char h;
+    cin>>m>>h>>d>>h>>y;
+    h = '.';
+    cout<<y<<h<<m<<h<<d;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
