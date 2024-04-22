4155fad916087d86451200d6ab8f5e1d08f63e1c
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-04-22 18:49:51 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240422/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<fixed;
+    double a, b, c;
+    cout.precision(3);
+    cin>>a>>b>>c;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    return 0;
+}
