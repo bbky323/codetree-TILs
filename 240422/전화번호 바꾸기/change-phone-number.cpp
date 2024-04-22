2f37df0ad83c7ef034b50f978a8f514273ff201b
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-04-22 19:22:46 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240422/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int num1, num2, num3;
+    char h;
+    cin>>num1>>h>>num2>>h>>num3;
+    cout<<"010"<<h<<num3<<h<<num2;
+    return 0;
+}
