58760e927bef3f509cd0fd74129dfe0389d53dfc
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-04-22 19:08:15 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240422/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    char h;
+    cin>>num1>>h>>num2;
+    cout<<num1<<num2;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
