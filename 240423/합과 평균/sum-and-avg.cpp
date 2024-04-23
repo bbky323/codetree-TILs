89b5290b165c370a37754670d538ede664010b60
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-04-23 23:45:14 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240423/합과 평균/sum-and-avg.cpp
+++ 240423/합과 평균/sum-and-avg.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main() {
-    double a, b;
+    cout<<fixed;
+    cout.precision(1);
+    int a, b;
     cin>>a>>b;
     double mean = (a + b) / 2.0;
     cout<<a+b<<" "<<mean;
