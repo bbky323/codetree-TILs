7deeb5fe61a1c553b528886b9b0a49b7dc109ad2
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-05-02 20:31:26 +0900

    Title: 정수 입력받아 배수 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240502/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, count=0;
+    cin>>n;
+    for(int i = 1; i<=100; i++){
+        if (i%n==0){
+            cout<<i<<" ";
+            count += 1;
+        }
+        if(count == 5){
+            break;
+        }
+
+    }
+    return 0;
+}
