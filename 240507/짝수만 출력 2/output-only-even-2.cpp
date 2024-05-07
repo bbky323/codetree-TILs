4830fcdbbf4e9a7c4764613bf0b8cf6b124bd9c4
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-05-07 18:00:54 +0900

    Title: 짝수만 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240507/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin>>b>>a;
+    while(b>=a){
+        if(b%2==0){
+            cout<<b<<" ";
+            b-=2;
+        }
+    }
+    return 0;
+}
