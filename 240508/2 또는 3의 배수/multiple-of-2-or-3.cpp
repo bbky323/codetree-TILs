70d5c09a285ca05d7413d92b430d38d5bccd8e2f
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-05-08 14:04:05 +0900

    Title: 2 또는 3의 배수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240508/2 또는 3의 배수/multiple-of-2-or-3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1; i<n+1; i++){
+        if(i%2==0){
+            cout<<1;
+            cout<<" ";
+        }
+        else if(i%3==0){
+            cout<<1;
+            cout<<" ";
+        }
+        else{
+            cout<<0;
+            cout<<" ";
+        }
+    }
+    return 0;
+}
