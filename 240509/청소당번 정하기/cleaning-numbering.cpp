d41d98a02ed27eb95e121e9fd69f2832a769cf40
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-05-09 15:10:05 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240509/청소당번 정하기/cleaning-numbering.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, class1=0, aisle=0, toilet=0;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        if(i!=0){
+            if(i%12==0){
+                toilet+=1;
+            }
+            else if(i%3==0){
+                aisle+=1;
+            }
+            else if(i%2==0){
+                class1+=1;
+            }
+        }   
+    }
+    cout<<class1<<" "<<aisle<<" "<<toilet;
+    return 0;
+}
