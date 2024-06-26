dbd97650a9e88b18fc3cd3f4a6c4a218bb60ec0e
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-06-27 01:36:19 +0900

    Title: 규칙에 맞는 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240627/규칙에 맞는 출력/output-that-matches-the-rule.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin>>N;
+    for(int i=1; i<N+1; i++){
+        for(int j=0; j<i; j++){
+            cout<<N+1-i+j<<" ";
+        }
+        cout<<""<<endl;
+    }
+    return 0;
+}
