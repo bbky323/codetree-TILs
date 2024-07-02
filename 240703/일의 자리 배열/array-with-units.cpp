6fcdef779d3ce8270c6e59d5289350f6b72ed394
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-07-03 02:52:15 +0900

    Title: 일의 자리 배열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240703/일의 자리 배열/array-with-units.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    for(int i=0; i<2; i++){
+        cin>>arr[i];
+        cout<<arr[i]<<" ";
+    }
+    for(int i=2; i<10; i++){
+        arr[i]=arr[i-1]+arr[i-2];
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
