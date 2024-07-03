760285459ad7012a9b642b8c83bf11b8d022e1ea
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-07-04 03:02:32 +0900

    Title: 나눗셈의 나머지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240704/나눗셈의 나머지/remainder-of-division.cpp
+++ 240704/나눗셈의 나머지/remainder-of-division.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int a, b, arr[100], cnt_arr[100]={0}, i=0, sum=0;
+    int a, b, arr[100], cnt_arr[10]={0}, i=0, sum=0;
     cin>>a>>b;
     while (true){
         arr[i]=a%b;
@@ -12,12 +12,12 @@ int main() {
             }
         }
         a/=b;
-        if(a==0){
+        if(a<=1){
             break;
         }
         i++;
     }
-    for(int j=0; j<100; j++){
+    for(int j=0; j<10; j++){
         sum+=cnt_arr[j]*cnt_arr[j];
     }
     cout<<sum;
