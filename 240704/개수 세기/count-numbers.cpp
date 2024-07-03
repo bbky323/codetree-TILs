3a2f37c79d563e9c646048c4d8f191d66c733a7c
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-07-04 03:17:01 +0900

    Title: 개수 세기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240704/개수 세기/count-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, arr[100], cnt=0;
+    cin>>n>>m;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+        if(arr[i]==m){
+            cnt++;
+        }
+    }
+    cout<<m;
+    return 0;
+}
