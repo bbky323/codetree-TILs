c748c9fd7ea2cc8894ab7cc99bee2bebc3fc270c
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-07-05 01:49:35 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240705/연속부분수열일까/contiguous-array-or-not.cpp
+++ 240705/연속부분수열일까/contiguous-array-or-not.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int A[100], B[100], n1, n2, res=0;
+    int A[100], B[100], n1, n2, res=-1;
     cin>>n1>>n2;
     for(int i=0; i<n1; i++){
         cin>>A[i];
@@ -15,18 +15,25 @@ int main() {
     }
     else{
         for(int i=0; i<n1-n2+1; i++){ // 첫째 인덱스가 같은지 확인하는 반복문
-            int cnt=0;
-            if(A[i]==B[0]){
-                for(int j=1; j<n2; j++){
-                    if(A[i+j]==B[j]){
-                        cnt++;
-                    }
+            if(n2==1){
+                if(A[i]==B[0]){
+                    res=n2-1;
                 }
             }
-            if(cnt==n2-1){
-                res=cnt;
-                break;
-            }
+            else{
+                int cnt=0;
+                if(A[i]==B[0]){
+                    for(int j=1; j<n2; j++){// 첫째 인덱스가 같다면 그 뒤 배열 비교
+                        if(A[i+j]==B[j]){
+                        cnt++;
+                        }
+                    }
+                    if(cnt==n2-1){
+                        res=cnt;
+                        break;
+                    }
+                }
+            }           
         }
         if(res==n2-1){
             cout<<"Yes";
