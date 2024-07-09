5a7a763cee07c1a19701e7d92a5ce3f53142a150
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-07-10 01:33:44 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240710/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string arr[5]={"apple", "banana", "grape", "blueberry", "orange"};
+    char c;
+    int cnt=0;
+
+    cin>>c;
+
+    for(int i=0; i<5; i++){
+        for(int j=0; j<4; j++){
+            if(arr[i][2]==c || arr[i][3]==c){
+                cnt++;
+                cout<<arr[i]<<endl;
+                break;
+            }
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
