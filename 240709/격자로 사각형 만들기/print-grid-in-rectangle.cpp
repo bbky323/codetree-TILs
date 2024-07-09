af25d96336bf0e2f23f3f8ddf39a0c207cd846bf
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-07-09 15:20:39 +0900

    Title: 격자로 사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240709/격자로 사각형 만들기/print-grid-in-rectangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, arr[10][10];
+
+    cin>>n;
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 || j==0){
+                arr[i][j]=1;
+            }
+            else{
+                arr[i][j]=arr[i-1][j]+arr[i][j-1]+arr[i-1][j-1];
+            }
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<""<<endl;
+    }
+    return 0;
+}
