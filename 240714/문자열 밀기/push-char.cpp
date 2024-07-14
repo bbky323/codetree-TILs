9aa75f0a6f9cb116476f7244b8a56a0eddf31f17
bbky323 <127292182+bbky323@users.noreply.github.com>
2024-07-14 18:52:20 +0900

    Title: 문자열 밀기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240714/문자열 밀기/push-char.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+
+    cin>>s;
+
+    s=s.substr(1, s.length()-1)+s[0];
+
+    cout<<s;
+    return 0;
+}
